1a726e68842c350247e74106f4c55b1046db3f1f
surya <sivasurya101010@gmail.com>
2026-08-14 13:38:57 +0530

    feat: add custom user model

--- config/settings.py
+++ config/settings.py
@@ -128,3 +128,6 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+
+AUTH_USER_MODEL = "users.User"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class TimestampedModel(models.Model):
+    created_at=models.DateTimeField(auto_now_add=True,verbose_name="created at")
+    updated_at=models.DateTimeField(auto_now=True,verbose_name="updated at")
+
+    class meta:
+        abstract=True 
+
+class User(AbstractUser,TimestampedModel):
+
+    class role(models.TextChoices):
+        customer="CUSTOMER","Customer"
+        owner="OWNER","Owner"
+
+    role=models.CharField(max_length=20,choices=role.choices,default="CUSTOMER",verbose_name="user role")
+
+    def __str__(self):
+        return self.username
+
+
+    
